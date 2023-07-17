50b09cd7f4776f3fcae457c63e5794cca642d5e0
nadun <weerakkody.kn@gmail.com>
2023-07-17 14:03:59 +0530

    add simple funcionality

---
+++ app.js
@@ -0,0 +1,10 @@
+let valuePara = document.getElementById('value');
+let lengthValue = document.getElementById('length-values');
+let volumeValue = document.getElementById('volume-values');
+let massValue = document.getElementById('mass-values');
+
+mainValue = document.getElementById('textInput').value;
+valuePara.innerHTML = mainValue;
+lengthValue.innerHTML = `${mainValue} meters = ${(mainValue * 3.281).toFixed(2)} feet | ${mainValue} feet = ${(mainValue / 3.281).toFixed(2)} meters`;
+volumeValue.innerHTML = `${mainValue} liters = ${(mainValue / 3.785).toFixed(2)} gallons | ${mainValue} gallons = ${(mainValue * 3.785).toFixed(2)} liters`;
+massValue.innerHTML = `${mainValue} kilos = ${(mainValue * 2.205).toFixed(2)} pounds | ${mainValue} pounds = ${(mainValue / 2.205).toFixed(2)} kilos`;
